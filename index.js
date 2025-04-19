1b443bcfe0248e643ac57db45a70d934e4f15ac8
Manojkumar270 <manojkumarp2705@gmail.com>
2025-04-19 20:09:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT|| 7878;
+const Mail = require("./mail");
+app.use(express.json());
+const fs = require("fs");
+const path = require("path");
+
+app.post("/mail", async (req, res) => {
+  //   console.log(req.body);
+  const { receiverid, subject, text, name } = req.body;
+  let fileLocation = fs.readFileSync(path.join(__dirname, "mail.html"),"utf8");
+  fileLocation = fileLocation.replace("[name]", name);
+  const mail = new Mail();
+  mail.setTo(receiverid);
+  mail.setSubject(subject);
+  mail.setText(text);
+  mail.setHtml(fileLocation);
+  mail.setCC("mk4539587@gmail.com")
+  mail.setBCC("manojkumarp2705@gmail.com")
+  mail.send();
+  res.send("Email sent!");
+});
+
+
+app.listen(PORT, () =>
+  console.log("connexted to port http://localhost:" + PORT)
+);
---
+++ mail.js
@@ -0,0 +1,72 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.ethereal.email",
+  secure: false,
+  port: 587,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+class Mail {
+  constructor() {
+    this.mailOptions = {
+      from: {
+        address: process.env.EMAIL,
+        name: "manoj",
+      },
+    };
+  }
+
+  setCompanyName(name) {
+    this.mailOptions.from.name = name;
+  }
+
+  setSenderEmail(email) {
+    this.mailOptions.from.address = email;
+  }
+
+  setTo(receiver) {
+    let receivers=this.mailOptions.to||[]
+    receivers.push(receiver)
+    this.mailOptions.to = receivers;
+  }
+
+  setCC(cc){
+    let ccs=this.mailOptions.cc||[]
+    ccs.push(cc)
+    this.mailOptions.cc = ccs;
+  }
+  setBCC(bcc){
+    let bccs=this.mailOptions.bcc||[]
+    bccs.push(bcc)
+    this.mailOptions.bcc = bccs;
+  }
+
+  setSubject(subject) {
+    this.mailOptions.subject = subject;
+  }
+
+  setText(text) {
+    this.mailOptions.text = text;
+  }
+
+  setHtml(html) {
+    this.mailOptions.html = html;
+  }
+
+  send() {
+    transporter.sendMail(this.mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("email sent successfully" + info.response);
+      }
+    });
+  }
+}
+
+module.exports = Mail;
